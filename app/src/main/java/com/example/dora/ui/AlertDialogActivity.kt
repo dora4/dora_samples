b67e972b024f5e90ff334f184f1574f6bcef67af
dora <dora924666990@gmail.com>
2025-01-08 23:15:04 +0800

    优化控件教程

--- app/src/main/java/com/example/dora/ui/AlertDialogActivity.kt
+++ app/src/main/java/com/example/dora/ui/AlertDialogActivity.kt
@@ -3,15 +3,10 @@ package com.example.dora.ui
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.dora.ARouterPath
-
-import dora.BaseActivity
-
 import com.example.dora.R
-import com.example.dora.MessageEvent
 import com.example.dora.databinding.ActivityAlertDialogBinding
+import dora.BaseActivity
 import dora.widget.DoraAlertDialog
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 
 @Route(path = ARouterPath.ACTIVITY_ALERT_DIALOG)
 class AlertDialogActivity : BaseActivity<ActivityAlertDialogBinding>() {
