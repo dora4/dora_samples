7d2e18a0c91848ce4225b8d0af8e53ef785d70da
dora <dora924666990@gmail.com>
2025-07-14 02:44:44 +0800

    添加一些封面和插画图片

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 31
-        versionName = "1.4.15"
+        versionCode = 32
+        versionName = "1.5.0"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
