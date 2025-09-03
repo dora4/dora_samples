b6f33375ad0bd468c00ce6844c3511fe5ebe940d
dora <dora924666990@gmail.com>
2025-09-03 13:33:10 +0800

    升级部分sdk版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 32
-        versionName = "1.5.0"
+        versionCode = 33
+        versionName = "1.5.1"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
