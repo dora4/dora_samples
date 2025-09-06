46a67fc76450b5d7a7f02dd3100367b3eb6ae658
dora <dora924666990@gmail.com>
2025-09-06 18:58:28 +0800

    新增头像裁剪

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 46
-        versionName = "1.5.14"
+        versionCode = 45
+        versionName = "1.5.13"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
