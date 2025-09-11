ce8f73a6dc8bfcdf5162597797f9152e33a955c0
dora <dora924666990@gmail.com>
2025-09-12 00:34:40 +0800

    优化界面

--- dview/src/main/java/com/example/dview/ui/AnimatorActivity.kt
+++ dview/src/main/java/com/example/dview/ui/AnimatorActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dview.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.common.ARouterPath
@@ -8,6 +9,8 @@ import dora.BaseActivity
 
 import com.example.dview.R
 import com.example.dview.databinding.ActivityAnimatorBinding
+import dora.util.IntentUtils
+import dora.util.StatusBarUtils
 import dora.widget.action.AlphaAction
 import dora.widget.action.CubicTo
 import dora.widget.action.LineTo
@@ -20,6 +23,13 @@ class AnimatorActivity : BaseActivity<ActivityAnimatorBinding>() {
 
     override fun getLayoutId(): Int {
         return R.layout.activity_animator
+    }
+
+    override fun onGetExtras(action: String?, bundle: Bundle?, intent: Intent) {
+        mBinding.titleBar.title = IntentUtils.getStringExtra(intent, "title")
+        val themeColor = IntentUtils.getIntExtra(intent, "themeColor")
+        mBinding.titleBar.setBackgroundColor(themeColor)
+        StatusBarUtils.setStatusBar(this, themeColor)
     }
 
     override fun initData(savedInstanceState: Bundle?, binding: ActivityAnimatorBinding) {
