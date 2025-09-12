7695bb0f1afd3c19f4f9974318d01056275e0d94
dora <dora924666990@gmail.com>
2025-09-12 16:00:01 +0800

    优化界面

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 49
-        versionName = "1.5.17"
+        versionCode = 50
+        versionName = "1.5.18"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
