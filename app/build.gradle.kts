a7d77d551b276087de55ec90d919a0feb9ae8189
dora <dora924666990@gmail.com>
2025-09-27 00:27:01 +0800

    新增运行时权限申请

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 65
-        versionName = "1.6.6"
+        versionCode = 66
+        versionName = "1.6.7"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
