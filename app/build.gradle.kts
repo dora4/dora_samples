934da12a5d992031cd55e4b07de2f77a05f3c7e4
dora <dora924666990@gmail.com>
2025-10-14 18:01:04 +0800

    升级支付SDK版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 34
-        versionCode = 68
-        versionName = "1.6.9"
+        versionCode = 69
+        versionName = "1.6.10"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
