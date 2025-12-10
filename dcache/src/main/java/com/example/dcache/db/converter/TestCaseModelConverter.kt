5ec53a08ddcb8f0dc50c7d8f4945a11673ca6ec6
Dora Chat <dora924666990@gmail.com>
2025-12-10 14:44:15 +0700

    更新细节

--- dcache/src/main/java/com/example/dcache/db/converter/TestCaseModelConverter.kt
+++ dcache/src/main/java/com/example/dcache/db/converter/TestCaseModelConverter.kt
@@ -4,6 +4,9 @@ import com.example.dcache.db.model.TestCaseModel
 import dora.db.converter.BaseJsonConverter
 import dora.db.converter.PropertyConverter
 
+/**
+ * PropertyConverter<TestCaseModel, String>接口也要实现，用于被反射监测到。
+ */
 class TestCaseModelConverter : BaseJsonConverter<TestCaseModel>(),
     PropertyConverter<TestCaseModel, String> {
 }
