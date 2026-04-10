abde7b92d2ccd5ec485db961164b83c5d55b60f1
Dora Chat <dora924666990@gmail.com>
2026-04-11 01:52:02 +0700

    升级部分sdk版本

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -46,21 +46,21 @@ dependencies {
     api("com.github.dora4:dora:1.3.61")
     api("com.github.dora4:dora-firebase-support:1.14")
     api("com.github.dora4:dora-brvah-support:1.6")
-    api("com.github.dora4:dora-arouter-support:1.10")
+    api("com.github.dora4:dora-arouter-support:1.11")
     kapt("com.alibaba:arouter-compiler:1.5.2")
     api("com.github.dora4:dora-pgyer-support:1.10")
     api("com.github.dora4:dora-dagger-support:1.12")
     kapt("com.google.dagger:dagger-compiler:2.16")
-    api("com.github.dora4:dora-walletconnect-support:2.1.34") {
+    api("com.github.dora4:dora-walletconnect-support:2.1.35") {
         exclude(group = "com.madgag.spongycastle", module = "core")
     }
-    api("com.github.dora4:dcache-android:3.6.3")
+    api("com.github.dora4:dcache-android:3.6.10")
     api("com.github.dora4:dview-colors:1.1")
-    api("com.github.dora4:dview-titlebar:1.37")
+    api("com.github.dora4:dview-titlebar:1.39")
     api("com.github.dora4:dview-menu-panel:1.47")
     api("com.github.dora4:dview-empty-layout:1.12")
-    api("com.github.dora4:dview-swipe-layout:1.0")
-    api("com.github.dora4:dview-alert-dialog:1.34")
+    api("com.github.dora4:dview-swipe-layout:1.1")
+    api("com.github.dora4:dview-alert-dialog:1.35")
 
     // XXPermissions
     api("com.github.getActivity:XXPermissions:18.2")
