43620a88e5e3641f9369e060fe637bfb92ac717d
跟着哆啦学安卓 <71242257+dora4@users.noreply.github.com>
2026-06-06 19:15:38 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     buildToolsVersion = "34.0.0"
     defaultConfig {
         minSdk = 24
-        versionCode = 82
-        versionName = "1.6.23"
+        versionCode = 83
+        versionName = "1.6.24"
         applicationId = "com.example.dora"
         // 21以上虚拟机天然支持multidex
         multiDexEnabled = true
